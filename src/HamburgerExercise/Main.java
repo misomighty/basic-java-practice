8d54204c1f80e22c7d5ea1794166262aecbe9071
Jordan <this.codedev@gmail.com>
2018-02-07 21:43:04 -0600

    add validation logic to add more topics

--- src/HamburgerExercise/Main.java
+++ src/HamburgerExercise/Main.java
@@ -6,6 +6,7 @@ class Hamburger {
     private double basePrice = 5.00;
 
     private int totalExtras = 4;
+    private int currentExtras = 0;
     private String extra1 = null;
     private double extra1Price = 0;
     private String extra2 = null;
@@ -42,20 +43,52 @@ class Hamburger {
     }
 
     public void addExtra1(String name, double price) {
-        this.extra1 = name;
-        this.extra1Price = price;
+        if(canAdd()) {
+            ++currentExtras;
+            this.extra1 = name;
+            this.extra1Price = price;
+        } else {
+            System.out.println("Cannot add more toppings");
+        }
+
     }
     public void addExtra2(String name, double price) {
-        this.extra2 = name;
-        this.extra2Price = price;
+        if(canAdd()) {
+            ++currentExtras;
+            this.extra2 = name;
+            this.extra2Price = price;
+        } else {
+            System.out.println("Cannot add more toppings");
+        }
+
     }
     public void addExtra3(String name, double price) {
-        this.extra3 = name;
-        this.extra3Price = price;
+        if(canAdd()) {
+            ++currentExtras;
+            this.extra3 = name;
+            this.extra3Price = price;
+        } else {
+            System.out.println("Cannot add more toppings");
+        }
+
     }
     public void addExtra4(String name, double price) {
-        this.extra4 = name;
-        this.extra4Price = price;
+        if(canAdd()) {
+            ++currentExtras;
+            this.extra4 = name;
+            this.extra4Price = price;
+        } else {
+            System.out.println("Cannot add more toppings");
+        }
+
+    }
+
+    public boolean canAdd() {
+        if(currentExtras < totalExtras) {
+            return true;
+        }else {
+            return false;
+        }
     }
 
     public String getBun() {
@@ -109,7 +142,10 @@ class Hamburger {
 
 class Healthy extends Hamburger{
     private String meat = null;
-    private int totalExtras = 0;
+    private int totalExtras = 6;
+    private int currentExtras = 0;
+    private int totalHealthyExtras = 2;
+    private int currentHealthyExtras = 0;
     private double basePrice = 7.00;
 
     private String healthyExtra1 = null;
@@ -119,23 +155,52 @@ class Healthy extends Hamburger{
     private String healthyExtra3 = null;
     private double healthyExtra3Price = 0;
 
-    public Healthy(String meat, int totalExtras) {
-        super("Brown rye", meat, totalExtras);
+    public Healthy(String meat) {
+        super("Brown rye", meat, 6);
         this.meat = meat;
-        this.totalExtras = totalExtras;
+    }
+
+    @Override
+    public boolean canAdd() {
+        if(currentExtras < totalExtras && currentHealthyExtras < totalHealthyExtras) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void addHealthyExtra1(String name, double price) {
-        this.healthyExtra1 = name;
-        this.healthyExtra1Price = price;
+        if(canAdd()) {
+            ++currentExtras;
+            ++currentHealthyExtras;
+            this.healthyExtra1 = name;
+            this.healthyExtra1Price = price;
+        } else {
+            System.out.println("Cannot add more toppings");
+        }
+
     }
     public void addHealthyExtra2(String name, double price) {
-        this.healthyExtra2 = name;
-        this.healthyExtra2Price = price;
+        if(canAdd()) {
+            ++currentExtras;
+            ++currentHealthyExtras;
+            this.healthyExtra2 = name;
+            this.healthyExtra2Price = price;
+        } else {
+            System.out.println("Cannot add more toppings");
+        }
+
     }
     public void addHealthyExtra3(String name, double price) {
-        this.healthyExtra3 = name;
-        this.healthyExtra3Price = price;
+        if(canAdd()) {
+            currentExtras++;
+            currentHealthyExtras++;
+            this.healthyExtra3 = name;
+            this.healthyExtra3Price = price;
+        } else {
+            System.out.println("Cannot add more toppings");
+        }
+
     }
 
     public String itemize() {
@@ -201,25 +266,113 @@ class Healthy extends Hamburger{
     public double getHealthyExtra3Price() {
         return healthyExtra3Price;
     }
+}
+
+class Deluxe extends Hamburger {
+    private String bun = null;
+    private String meat = null;
+    private double basePrice = 10.00;
+    private int totalExtras = 0;
+
+    private String chips = null;
+    private double chipsPrice = 0;
+    private String drink = null;
+    private double drinkPrice = 0;
+
+    public Deluxe(String bun, String meat) {
+        super(bun, meat, 0);
+            this.bun = bun;
+            this.meat = meat;
+    }
+
+    public String itemize() {
+        String str =
+                "bun: " + getBun() + "\n" +
+                "meat: " + getMeat() + "\n";
+        double total = 0;
+        if(chips != null) {
+            total += getChipsPrice();
+            str += "added " + getChips() + " for an extra " + getChipsPrice() + "\n";
+        }if (drink != null) {
+            total += getDrinkPrice();
+            str += "added " + getDrink() + " for an extra " + getDrinkPrice() + "\n";
+        }
+
+        str += "Total: " + (total + basePrice);
+        return str;
+    }
+
+    public void addChips(String name, double price) {
+        this.chips = name;
+        this.chipsPrice = price;
+    }
+
+    public void addDrink(String name, double price) {
+        this.drink = name;
+        this.drinkPrice = price;
+    }
 
 
+
+    @Override
+    public String getBun() {
+        return bun;
+    }
+
+    @Override
+    public String getMeat() {
+        return meat;
+    }
+
+    @Override
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public String getChips() {
+        return chips;
+    }
+
+    public double getChipsPrice() {
+        return chipsPrice;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+
+    public double getDrinkPrice() {
+        return drinkPrice;
+    }
 }
 public class Main {
     public static void main(String[] args) {
-        Hamburger basic = new Hamburger("White", "Single", 4);
+        Hamburger basic = new Hamburger("white", "single", 4);
         basic.addExtra1("lettuce", 0.25);
         basic.addExtra2("lettuce", 0.25);
         basic.addExtra3("lettuce", 0.25);
         basic.addExtra4("lettuce", 0.25);
+        basic.addExtra4("lettuce", 0.25);
         System.out.println(basic.itemize());
         System.out.println("------------------------------------------------------------");
 
-        Healthy healthy = new Healthy("black bean", 6);
+        Healthy healthy = new Healthy("black bean");
         healthy.addHealthyExtra1("avocado", 3.50);
         healthy.addHealthyExtra2("quinoa", 3.50);
         healthy.addHealthyExtra3("flax", 3.50);
+        healthy.addExtra1("tomato", 0.50);
+        healthy.addExtra2("tomato", 0.50);
+        healthy.addExtra3("tomato", 0.50);
+        healthy.addExtra4("tomato", 0.50);
         System.out.println(healthy.itemize());
         System.out.println("------------------------------------------------------------");
+
+        Deluxe deluxe = new Deluxe("sesame", "triple");
+        deluxe.addChips("sun chips", 2.50);
+        deluxe.addDrink("lemon brisk", 2.50);
+        deluxe.addExtra1("tomato", 0.50);
+        System.out.println(deluxe.getExtra1());
+        System.out.println(deluxe.itemize());
 
     }
 }
